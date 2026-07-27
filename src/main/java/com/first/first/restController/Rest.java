5d678e9c004289c885f603ed55cba3379c83beb2
chetnadua008 <chetnadua36@gmail.com>
2026-07-27 22:51:26 +0530

    Rest Controller, Spring Actuators

---
+++ src/main/java/com/first/first/restController/Rest.java
@@ -0,0 +1,27 @@
+package com.first.first.restController;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class Rest {
+
+    //inject property from application.properties
+    @Value("${my.name}")
+    private String name;
+
+    //expose end points in rest controller
+    @GetMapping("/")
+    public String sendMessage(){
+        return name;
+    }
+
+    //new endpoint to test restart
+    @GetMapping("/test")
+    public String testRestart(){
+        return "Application restarts";
+    }
+}
---
+++ src/test/java/com/first/first/FirstApplicationTests.java
@@ -0,0 +1,12 @@
+package com.first.first;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
